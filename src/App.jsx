de9848bdedd37f2c4c6bc5ef82b87f786f2aff75
Demis Russo <demisjunior@hotmail.com>
2020-05-18 18:55:40 -0300

    infra pronta

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Header from './components/principal/Header';
+import Main from './components/principal/Main';
+import Footer from './components/principal/Footer';
+
+export default () => 
+    <div>
+        <Header />
+        <Main />
+        <Footer />
+    </div>
---
+++ src/components/principal/Main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default props => {
+    return (
+        <>
+            <main class="conteudo">Conteudo</main>
+        </>
+    )
+}
